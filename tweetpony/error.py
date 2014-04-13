186d3962f35899f7f7f6b51987841ec23065d975
Mayank Gupta <mayankgupta173@gmail.com>
2014-04-12 23:20:09 -0400

    improved AttrDict in Models, changed tabs to spaces in *

--- tweetpony/error.py
+++ tweetpony/error.py
@@ -2,17 +2,17 @@
 # See the LICENSE file for the full license.
 
 class APIError(Exception):
-	def __init__(self, code, description, body = None):
-		self.code = code
-		self.description = description
-		self.body = body
-	
-	def __str__(self):
-		return "#%i: %s" % (self.code, self.description)
+  def __init__(self, code, description, body = None):
+    self.code = code
+    self.description = description
+    self.body = body
+
+  def __str__(self):
+    return "#%i: %s" % (self.code, self.description)
 
 class ParameterError(Exception):
-	def __init__(self, description):
-		self.description = description
-	
-	def __str__(self):
-		return self.description
+  def __init__(self, description):
+    self.description = description
+
+  def __str__(self):
+    return self.description
--- tweetpony/utils.py
+++ tweetpony/utils.py
@@ -5,21 +5,21 @@ import re
 from urllib import quote as _quote
 
 def optimize_mentions(usernames, text):
-	username = re.compile(r'(?:^|[^\w]+)(?P<name>@\w+)')
-	existing_mentions = username.findall(text.lower())
-	missing_mentions = [name for name in usernames if name.lower() not in existing_mentions]
-	text = "%s %s" % (" ".join(["@%s" % name for name in missing_mentions]), text)
-	return text
+  username = re.compile(r'(?:^|[^\w]+)(?P<name>@\w+)')
+  existing_mentions = username.findall(text.lower())
+  missing_mentions = [name for name in usernames if name.lower() not in existing_mentions]
+  text = "%s %s" % (" ".join(["@%s" % name for name in missing_mentions]), text)
+  return text
 
 def quote(text, *args, **kwargs):
-	t = type(text)
-	if t is str:
-		converted_text = text
-	elif t is unicode:
-		converted_text = str(text.encode('utf-8'))
-	else:
-		try:
-			converted_text = str(text)
-		except:
-			converted_text = text
-	return _quote(converted_text, *args, **kwargs)
+  t = type(text)
+  if t is str:
+    converted_text = text
+  elif t is unicode:
+    converted_text = str(text.encode('utf-8'))
+  else:
+    try:
+      converted_text = str(text)
+    except:
+      converted_text = text
+  return _quote(converted_text, *args, **kwargs)
