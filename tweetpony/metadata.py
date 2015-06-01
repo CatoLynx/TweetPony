60981144c15939403225215617d24d5a1e2cb9de
Julian Metzler <git@mezgrman.de>
2015-06-01 19:04:40 +0200

    Bumped version

--- tweetpony/metadata.py
+++ tweetpony/metadata.py
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 name = "TweetPony"
-version = "1.5.1"
+version = "1.5.2"
 description = "A Twitter library for Python"
 license = "AGPLv3"
 author = "Julian Metzler"
